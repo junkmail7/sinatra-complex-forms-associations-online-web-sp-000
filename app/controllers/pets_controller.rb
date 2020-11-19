301174b0a898687a04d2f14712777eac2248e0e3
William Pannell <lpann5@gmail.com>
2020-11-19 21:54:52 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -25,7 +25,7 @@ class PetsController < ApplicationController
   end
 
   get '/pets/:id/edit' do
-    #@pet = Pet.find(params[:id])
+    @pet = Pet.find(params[:id])
     erb :'/pets/edit'
   end
 
