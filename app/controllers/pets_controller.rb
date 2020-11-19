b6a7f3c9b9f7af7f1f3d27a29bcc56a15cb90943
William Pannell <lpann5@gmail.com>
2020-11-19 22:01:53 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -30,6 +30,11 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do
+    ####### bug fix
+    if !params[:owner].keys.include?("pet_ids")
+    params[:owner]["pet_ids"] = []
+    end
+    #######
     @pet = Pet.find(params[:id])
     @pet.update(params["pet"])
     if !params["owner"]["name"].empty?
